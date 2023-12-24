9f9ee0179177a579ec295908638e424d39263f5a
UGBOR MOSES <udensimoses2@gmail.com>
2023-12-24 14:50:34 +0000

    Update web-app.sh

--- web-app.sh
+++ web-app.sh
@@ -10,7 +10,7 @@ echo "COPY ./templates /home/myapp/templates/" >>tempdir/Dockerfile
 echo "EXPOSE 5000 >> tempdir/Dockerfile"
 echo "CMD python /home/myapp/attendance_register.py" >>tempdir/Dockerfile
 cd tempdir
-sudo docker build -t attendance_register .
-sudo docker run -t -d -p 5000:5000 --name samplerunning attendance_register
-sudo docker ps -a
+docker build -t attendance_register .
+docker run -t -d -p 5000:5000 --name samplerunning attendance_register
+docker ps -a
 
