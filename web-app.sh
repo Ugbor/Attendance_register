9002c9239e5b111b5b3769cdc649b1c9fedb7a6b
UGBOR MOSES <udensimoses2@gmail.com>
2023-12-24 16:10:07 +0000

    Update web-app.sh

--- web-app.sh
+++ web-app.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
 cp attendance_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
+
 echo "From python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
@@ -10,8 +15,11 @@ echo "COPY ./templates /home/myapp/templates/" >>tempdir/Dockerfile
 echo "COPY attendance_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5000 >> tempdir/Dockerfile"
 echo "CMD python /home/myapp/attendance_register.py" >>tempdir/Dockerfile
+
 cd tempdir
+
 docker build -t attendance_register .
+
 docker run -t -d -p 5000:5000 --name samplerunning attendance_register
 docker ps -a
 
