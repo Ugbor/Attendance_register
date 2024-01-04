629ca3df71a660a20115a0d1b8d0cd63a76ae3db
UGBOR MOSES <udensimoses2@gmail.com>
2024-01-04 14:58:40 +0000

    Update web-app.sh

--- web-app.sh
+++ web-app.sh
@@ -14,12 +14,12 @@ echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY ./templates /home/myapp/templates/" >>tempdir/Dockerfile
 echo "COPY attendance_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5000 >> tempdir/Dockerfile"
-echo "CMD python /home/myapp/attendance_register.py" >>tempdir/Dockerfile
+echo "CMD python /home/myapp/attendance_app.py" >>tempdir/Dockerfile
 
 cd tempdir
 
 docker build -t attendance_register .
 
-docker run -t -d -p 5000:5000 --name samplerunning attendance_register
+docker run -t -d -p 5000:5000 --name samplerunning attendance_app
 docker ps -a
 
